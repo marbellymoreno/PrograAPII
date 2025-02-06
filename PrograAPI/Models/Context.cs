69385168e0c25476729ddc41532f03aa5671866e
marbellymoreno <me0763032023@unab.edu.sv>
2025-02-06 11:21:55 -0600

    Avance

---
+++ PrograAPI/Models/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PrograAPI.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) {}
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ PrograAPI/Models/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrograAPI.Models
+{
+    public class Producto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Precio { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Stock { get; set; }
+    }
+}
